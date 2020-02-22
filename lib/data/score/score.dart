c95acfd17ab7d022a801250bf54d57a6ee4914e7
YourAverageMoron <34078850+YourAverageMoron@users.noreply.github.com>
2020-02-22 16:03:44 +0000

    Use JSON to store the scaled score times
    Alls that to be passed into a flutter object

--- lib/data/score/score.dart
+++ lib/data/score/score.dart
@@ -1,16 +1,47 @@
 import 'package:dissertation_project/data/phone_usage/app_usage_time.dart';
-import 'package:dissertation_project/data/time_scaler/time_scaler.dart';
-import 'package:flutter/material.dart';
+import 'package:dissertation_project/data/time_scaler/scaled_score_time.dart';
 
-// TODO JUST USE JSON OBJECTS SHARED PREFERENCES?
-final doubleTimes = [new TimeScaler(TimeOfDay(hour:4, minute: 0), TimeOfDay(hour:5,minute: 0), 5)];
+// TODO Store this as a shared preference
+var timeOfDay = [
+  {
+    'startTime': {
+      'hour': 4,
+      'minute':0,
+    },
+    'endTime': {
+      'hour': 5,
+      'minute':0,
+    },
+    'scaleFactor': 0.5
+  },
+  {
+    'startTime': {
+      'hour': 14,
+      'minute':0,
+    },
+    'endTime': {
+      'hour': 16,
+      'minute':0,
+    },
+    'scaleFactor': 5.0
+  },
+];
 
 // TODO Static?
 class Score {
 
+  static List<ScaledScoreTime> _getScaledScoreTimes() {
+    //TODO you will need to get the scaled score from shared preferences
+
+    return timeOfDay.map((json) => ScaledScoreTime.fromJson(json)).toList();
+    }
 
   //TODO handle different time periods in this
-  static Future<int> _getTotalTimeScore(DateTime date) async{
+  static Future<int> _getTotalTimeScore(DateTime date) async {
+
+    List<ScaledScoreTime> SST = _getScaledScoreTimes();
+
+    SST.forEach((time) => print("${time.getStartTime()} ${time.getEndTime()} + ${time.getScaleFactor()}"));
 
     int year = date.year;
     int month = date.month;
@@ -21,9 +52,11 @@ class Score {
 
     double totalTime = 0;
 
-    Map<String, double> usageTimes = await AppUsageTime.getUsageStats(startTime, endTime);
+    Map<String, double> usageTimes = await AppUsageTime.getUsageStats(
+        startTime, endTime);
 
-    usageTimes.forEach((name, time) => {
+    usageTimes.forEach((name, time) =>
+    {
       totalTime += time // TODO CHECK THAT THIS IS THE BEST WAY TO DO THIS
     });
 
@@ -31,17 +64,14 @@ class Score {
   }
 
 
-  static int _calculateTotalTimeScore(double timeUsed){
+  static int _calculateTotalTimeScore(double timeUsed) {
     // TODO THIS IS JUST DOING THE PERCENTAGE TIME USED
-    print(((1 - timeUsed/86400)*100).round());
-    return ((1 - timeUsed/86400)*100).round();
+    print(((1 - timeUsed / 86400) * 100).round());
+    return ((1 - timeUsed / 86400) * 100).round();
   }
 
 
-  static Future<int> generateScore(DateTime date){
+  static Future<int> generateScore(DateTime date) {
     return _getTotalTimeScore(date);
   }
-
-
-
 }
