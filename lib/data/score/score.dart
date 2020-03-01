8caf48f809b8ede4f81df3a9399022edcc96087a
YourAverageMoron <34078850+YourAverageMoron@users.noreply.github.com>
2020-03-01 12:31:32 +0000

    Score now generates a percentage of time (needs to be include the other values but its a start)

--- lib/data/score/score.dart
+++ lib/data/score/score.dart
@@ -1,19 +1,39 @@
 import 'package:dissertation_project/data/phone_usage/phone_usage_statistics.dart';
 import 'package:dissertation_project/data/shared_preferences/scaled_score_times_preferences.dart';
+import 'package:dissertation_project/data/time_scaler/comparable_time_of_day.dart';
 import 'package:dissertation_project/data/time_scaler/scaled_score_time.dart';
 import 'package:dissertation_project/helpers/datetime_helpers.dart';
 
 class Score {
-  //TODO create a constructor?
-  //TODO MAYBE HAVE THE VARIABLES STORED IN THIS, THEN UPDATE THEM WHEN ACCESSING? -> might be more efficient as you can count stuff in one loop
+
   final PhoneUsageStatistics _phoneUsageStatistics = PhoneUsageStatistics();
   final ScaledScoreTimesPreferences _scaledScoreTimesPreferences =
-      ScaledScoreTimesPreferences();
+  ScaledScoreTimesPreferences();
   final DateTimeHelpers _dateTimeHelpers = DateTimeHelpers();
 
-  Future<int> _getTotalTimeScore(DateTime date) async {
+  double _totalTime = 0;
+  double _totalScreenTime = 0;
+  double _totalApplicationsOpened = 0;
+
+
+  Future<int> generateScore(DateTime date) async {
+    await _updateScoreParameters(date);
+
+    return _calculateScore();
+  }
+
+  int _calculateScore() {
+    return (_totalScreenTime / _totalTime).round().toInt();
+  }
+
+  //TODO THIS NEEDS TO FACTOR FOR THAT BEING IN THE MIDDLE OF THE DAY
+  //todo it current just looks at the total time in the day
+  //todo THIS MIGHT want to do this in the get preferences thing
+  Future<void> _updateScoreParameters(DateTime date) async {
+    ComparableTimeOfDay endTime = ComparableTimeOfDay(
+        hour: date.hour, minute: date.minute);
     List<ScaledScoreTime> scaledScoreTimeList =
-        await _scaledScoreTimesPreferences.getAllTimesScored();
+    await _scaledScoreTimesPreferences.getAllTimesScored(endTime);
 
     double totalTime = 0;
     double totalScreenTime = 0;
@@ -21,27 +41,20 @@ class Score {
 
     for (ScaledScoreTime scaledScoreTime in scaledScoreTimeList) {
       DateTime startDate =
-          _dateTimeHelpers.timeOfDayToDate(date, scaledScoreTime.getStartTime());
+      _dateTimeHelpers.timeOfDayToDate(date, scaledScoreTime.getStartTime());
       DateTime endDate =
-          _dateTimeHelpers.timeOfDayToDate(date, scaledScoreTime.getEndTime());
+      _dateTimeHelpers.timeOfDayToDate(date, scaledScoreTime.getEndTime());
       double scaleFactor = scaledScoreTime.getScaleFactor();
 
       totalTime += scaledScoreTime.calculateTimeDifference() * scaleFactor;
       totalApplicationsOpened += await _phoneUsageStatistics
           .getTotalApplicationOpens(startDate, endDate,
-              scaleFactor: scaleFactor);
+          scaleFactor: scaleFactor);
       totalScreenTime += await _phoneUsageStatistics
           .getTotalAppUsageTime(startDate, endDate, scaleFactor: scaleFactor);
     }
-    // Sum of the open numbers
-    print('Total: $totalTime');
-    print('Screentime: $totalScreenTime');
-    print('Opens: $totalApplicationsOpened');
-
-    return null;
-  }
-
-  Future<int> generateScore(DateTime date) {
-    return _getTotalTimeScore(date);
+    _totalTime = totalTime;
+    _totalScreenTime = totalScreenTime;
+    _totalApplicationsOpened = totalApplicationsOpened;
   }
 }
