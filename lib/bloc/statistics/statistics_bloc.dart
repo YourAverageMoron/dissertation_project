d3f121b2cb5106d6c76b60a00099c0fb9a48abec
YourAverageMoron <34078850+YourAverageMoron@users.noreply.github.com>
2020-03-13 11:04:23 +0000

    Pie chart now uses the app name instead of the package name

--- lib/bloc/statistics/statistics_bloc.dart
+++ lib/bloc/statistics/statistics_bloc.dart
@@ -4,6 +4,7 @@ import 'package:dissertation_project/bloc/statistics/statistics_state.dart';
 import 'package:dissertation_project/data/phone_usage/app_usage_statistic.dart';
 import 'package:dissertation_project/data/phone_usage/get_app_usage_times.dart';
 import 'package:dissertation_project/data/phone_usage/phone_usage_statistics.dart';
+import 'package:dissertation_project/helpers/system_packages_info/package_manager_repository.dart';
 import 'package:dissertation_project/kiwi_di/injector.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -11,6 +12,8 @@ class StatsBloc extends Bloc<StatsEvent, StatsState> {
   final PhoneUsageStatistics _phoneUsageStatistics =
       Injector.resolve<PhoneUsageStatistics>();
   final GetAppUsageTimes _appUsageTime = Injector.resolve<GetAppUsageTimes>();
+  PackageManagerRepository _packageManagerRepository =
+      Injector.resolve<PackageManagerRepository>();
 
   @override
   StatsState get initialState => StatsEmpty();
@@ -53,20 +56,25 @@ class StatsBloc extends Bloc<StatsEvent, StatsState> {
 
   List<charts.Series> getAppScreenTimeBreakdown(
       Map<String, AppUsageStat> appUsageStats) {
-    List<AppUsageStat> data = [];
-    appUsageStats.forEach((key, value) {
-      data.add(value);
+    List<Map<String, AppUsageStat>> data = [];
+    appUsageStats.forEach((key, value) async {
+      data.add({
+        await _packageManagerRepository
+            .getPackageInfo(value.getPackageName())
+            .then((value) => value.appName): value
+      });
     });
+
     return [
-      new charts.Series<AppUsageStat, int>(
+      new charts.Series<Map<String, AppUsageStat>, int>(
         id: 'AppScreenTimeBreakdown',
-        domainFn: (AppUsageStat stat, _) =>
-            stat.getTimeInForground().round().toInt(),
-        measureFn: (AppUsageStat stat, _) => stat.getTimeInForground(),
+        domainFn: (Map<String, AppUsageStat> stat, _) =>
+            stat.values.first.getTimeInForground().round().toInt(),
+        measureFn: (Map<String, AppUsageStat> stat, _) =>
+            stat.values.first.getTimeInForground(),
         data: data,
-        // Set a label accessor to control the text of the arc label.
-        labelAccessorFn: (AppUsageStat row, _) =>
-            '${row.getPackageName()}: ${row.getTimeInForground()}',
+        labelAccessorFn: (Map<String, AppUsageStat> row, _) =>
+            '${row.keys.first}',
       )
     ];
   }
